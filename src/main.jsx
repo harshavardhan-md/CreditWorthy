f17b6aa6b87aa9a92cf0886f4e1756d97c3a7cb9
Harshavardhan M <136354445+harshavardhan-md@users.noreply.github.com>
2024-11-24 00:54:19 +0530

    update to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react';
-
-const clerk_key = import.meta.env.VITE_CLERK_KEY;
-
-if(!clerk_key){
-  throw new Error("Key was not found");
-}
+import { config } from './config'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={clerk_key}>
-    <App />
+    <ClerkProvider publishableKey={config.clerkKey}>
+      <App />
     </ClerkProvider>
   </StrictMode>,
 )
